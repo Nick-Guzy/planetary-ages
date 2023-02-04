fc8a508c3342ca7984e9c365ee3326d6dad0a5b6
nicksfed <nick.guzy76@gmail.com>
2023-02-04 14:16:41 -0800

    first test good fail

--- src/__tests__/age.tests.js
+++ src/__tests__/age.tests.js
@@ -1,21 +1,23 @@
-import age from '../age';
+import Person from '../age';
 
 describe('Age', () => {
 
 test("Should correctly construct a person with a past, current, and future age", () => { 
-Code: 
-expect()
-});
-  test("To accurately calculate what age you currently are on each planet", () => {
-    current age planet = age(Mer, Ven, Mar, Jup)
-    expect()
+const person = new Person(31, 28, 33);
+  expect(person.currentAge).toEqual(31);
+  expect(person.pastAge).toEqual(28);
+  expect(person.futureAge).toEqual(33);
   });
-  test("To accurately calculate how long until your next birthday on each planet", () => {
-    Code: 
-    expect()
-  });
-  test("To accurately calculate how long since your last birthday on each planet", () => {
-    Code:
-    expect()
-  });
+  // test("To accurately calculate what age you currently are on each planet", () => {
+  //   current age planet = age(Mer, Ven, Mar, Jup)
+  //   expect()
+  // });
+  // test("To accurately calculate how long until your next birthday on each planet", () => {
+  //   Code: 
+  //   expect()
+  // });
+  // test("To accurately calculate how long since your last birthday on each planet", () => {
+  //   Code:
+  //   expect()
+  // });
 });
--- src/age.js
+++ src/age.js
@@ -1,8 +1,8 @@
 export default class Person {
   constructor(currentAge, pastAge, futureAge) {
-  this.currentAge = currentAge;
-  this.pastAge = pastAge;
-  this.futureAge = futureAge;
+  // this.currentAge = currentAge;
+  // this.pastAge = pastAge;
+  // this.futureAge = futureAge;
 }
 
 checkType() {
