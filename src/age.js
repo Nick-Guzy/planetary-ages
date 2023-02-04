5ca416b011779de0e4422c8cb6874ff194c2b743
nicksfed <nick.guzy76@gmail.com>
2023-02-03 16:29:00 -0800

    WIP writing tests, then logic

---
+++ src/age.js
@@ -0,0 +1,27 @@
+export default class Person {
+  constructor(currentAge, pastAge, futureAge) {
+  this.currentAge = currentAge;
+  this.pastAge = pastAge;
+  this.futureAge = futureAge;
+}
+
+checkType() {
+  currMerAge = this.currentAge / .24;
+  currVenAge = this.currentAge / .62;
+  currMarAge = this.currentAge * 1.88;
+  currJupAge = this.currentAge * 11.86;
+  futureMerAge = this.futureAge /.24;
+  futureVenAge = this.futureAge / .62;
+  futureMarAge = this.futureAge * 1.88;
+  futureJupAge = this.futureAge * 11.86;
+  pastMerAge = this.pastAge / .24;
+  pastVenAge = this.pastAge / .62;
+  pastMarAge = this.pastAge * 1.88;
+  pastJupAge = this.pastAge * 11.86;
+
+    
+
+  }
+}
+
+
