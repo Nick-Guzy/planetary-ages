59fa75cfd87a8ddb6898a379d2c35e38b6b7c30f
nicksfed <nick.guzy76@gmail.com>
2023-02-04 16:11:27 -0800

    second test good fail with new class constructor

--- src/__tests__/age.tests.js
+++ src/__tests__/age.tests.js
@@ -1,5 +1,5 @@
 import Person from '../age';
-
+import Planet from '../age';
 describe('Age', () => {
 
 test("Should correctly construct a person with a past, current, and future age", () => { 
@@ -9,16 +9,8 @@ const person = new Person(31, 28, 33);
   expect(person.futureAge).toEqual(33);
   });
   
-  test("To accurately calculate what age you currently are on Mercury", () => {
-    const currentPerson = new Person(31, 28, 33);
-    expect(currentPerson.checkType()).toBeCloseTo(129.16666666666669);
-  });
-  // test("To accurately calculate how long until your next birthday on each planet", () => {
-  //   Code: 
-  //   expect()
-  // });
-  // test("To accurately calculate how long since your last birthday on each planet", () => {
-  //   Code:
-  //   expect()
-  // });
+test("Should correctly construct Mercury and tell you how old you are on Mercury", () => {
+  const Mer = new Planet("Mer", .24)
+  expect(Mer.name).toEqual("Mer");
+      });
 });
--- src/age.js
+++ src/age.js
@@ -4,25 +4,13 @@ export default class Person {
   this.pastAge = pastAge;
   this.futureAge = futureAge;
 }
-
-checkType() {
-  const currMerAge = this.currentAge / .24;
-  return currMerAge;
-  // currVenAge = this.currentAge / .62;
-  // currMarAge = this.currentAge * 1.88;
-  // currJupAge = this.currentAge * 11.86;
-  // futureMerAge = this.futureAge /.24;
-  // futureVenAge = this.futureAge / .62;
-  // futureMarAge = this.futureAge * 1.88;
-  // futureJupAge = this.futureAge * 11.86;
-  // pastMerAge = this.pastAge / .24;
-  // pastVenAge = this.pastAge / .62;
-  // pastMarAge = this.pastAge * 1.88;
-  // pastJupAge = this.pastAge * 11.86;
-
-    
-
-  }
-}
-
-
+};
+export class Planet {
+  constructor(name, ageModifier) {
+  this.name = name;
+  this.ageModifier = ageModifier
+  // this.Ven = Ven;
+  // this.Mar = Mar;
+  // this.Jup = Jup;
+    }
+  };
